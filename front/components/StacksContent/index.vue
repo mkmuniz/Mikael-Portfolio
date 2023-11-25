1615305330988dba40eaeccb947f451fee150818
Mikael <mikaelmuniz2001@gmail.com>
2023-11-25 11:19:25 -0300

    feat: skills

--- front/components/StacksContent/index.vue
+++ front/components/StacksContent/index.vue
@@ -1,35 +1,73 @@
 <template>
-    <div class="flex flex-col items-center justify-center w-full h-80 bg-white text-black font-hubot">
-        <div class="w-4/6">
-            <div class="flex justify-between mb-1">
-                <span class="text-base font-medium">TypeScript</span>
-            </div>
-            <div class="w-full bg-gray-200 rounded-full h-1.5 dark:bg-gray-700">
-                <div class="bg-standard h-1.5 rounded-full w-full"></div>
-            </div>
-        </div>
-        <div class="w-4/6">
-            <div class="flex justify-between mb-1">
-                <span class="text-base font-medium">Golang</span>
-            </div>
-            <div class="w-full bg-gray-200 rounded-full h-1.5 dark:bg-gray-700">
-                <div class="bg-standard h-1.5 rounded-full w-full"></div>
+    <div class="grid grid-cols-2 w-full h-screen bg-white font-hubot">
+        <div class="flex flex-col w-full items-center justify-center">
+            <span class="text-4xl mb-12">
+                ABOUT MY CAREER & SKILLS
+            </span>
+            <div class="text-center">
+                <p>
+                    Actually, I have more than 3 years experience as software engineer. Working mainly
+                    with NodeJS, React (and his frameworks like NextJS), GoLang & TypeScript. Also, another
+                    techonologies like C#, Python, AWS, Kubernetes & DevOps in general.
+                </p>
             </div>
         </div>
-        <div class="w-4/6">
-            <div class="flex justify-between mb-1">
-                <span class="text-base font-medium">NodeJS</span>
+        <div class="flex flex-col items-center justify-center w-full h-full text-black font-hubot">
+            <div class="w-4/6">
+                <div class="flex justify-between mb-1">
+                    <span class="text-base font-medium">NodeJS</span>
+                </div>
+                <div class="w-full bg-gray-200 rounded-full h-1.5 dark:bg-gray-700">
+                    <div class="bg-standard h-1.5 rounded-full w-full"></div>
+                </div>
             </div>
-            <div class="w-full bg-gray-200 rounded-full h-1.5 dark:bg-gray-700">
-                <div class="bg-standard h-1.5 rounded-full w-full"></div>
+            <div class="w-4/6">
+                <div class="flex justify-between mb-1">
+                    <span class="text-base font-medium">TypeScript</span>
+                </div>
+                <div class="w-full bg-gray-200 rounded-full h-1.5 dark:bg-gray-700">
+                    <div class="bg-standard h-1.5 rounded-full w-full"></div>
+                </div>
             </div>
-        </div>
-        <div class="w-4/6">
-            <div class="flex justify-between mb-1">
-                <span class="text-base font-medium">NextJS</span>
+            <div class="w-4/6">
+                <div class="flex justify-between mb-1">
+                    <span class="text-base font-medium">NextJS</span>
+                </div>
+                <div class="w-full bg-gray-200 rounded-full h-1.5 dark:bg-gray-700">
+                    <div class="bg-standard h-1.5 rounded-full w-full"></div>
+                </div>
             </div>
-            <div class="w-full bg-gray-200 rounded-full h-1.5 dark:bg-gray-700">
-                <div class="bg-standard h-1.5 rounded-full w-full"></div>
+            <div class="w-4/6">
+                <div class="flex justify-between mb-1">
+                    <span class="text-base font-medium">Docker</span>
+                </div>
+                <div class="w-full bg-gray-200 rounded-full h-1.5 dark:bg-gray-700">
+                    <div class="bg-standard h-1.5 rounded-full w-full"></div>
+                </div>
+            </div>
+            <div class="w-4/6">
+                <div class="flex justify-between mb-1">
+                    <span class="text-base font-medium">GoLang</span>
+                </div>
+                <div class="w-full bg-gray-200 rounded-full h-1.5 dark:bg-gray-700">
+                    <div class="bg-standard h-1.5 rounded-full w-11/12"></div>
+                </div>
+            </div>
+            <div class="w-4/6">
+                <div class="flex justify-between mb-1">
+                    <span class="text-base font-medium">Kubernetes</span>
+                </div>
+                <div class="w-full bg-gray-200 rounded-full h-1.5 dark:bg-gray-700">
+                    <div class="bg-standard h-1.5 rounded-full w-10/12"></div>
+                </div>
+            </div>
+            <div class="w-4/6">
+                <div class="flex justify-between mb-1">
+                    <span class="text-base font-medium">AWS</span>
+                </div>
+                <div class="w-full bg-gray-200 rounded-full h-1.5 dark:bg-gray-700">
+                    <div class="bg-standard h-1.5 rounded-full w-9/12"></div>
+                </div>
             </div>
         </div>
     </div>
