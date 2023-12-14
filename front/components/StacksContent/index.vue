27fa8ee0b8ec284ffa74c1de741513ea8350c10c
Mikael <mikaelmuniz2001@gmail.com>
2023-12-14 19:14:08 -0300

    fix: experience

--- front/components/StacksContent/index.vue
+++ front/components/StacksContent/index.vue
@@ -14,36 +14,24 @@
         </div>
         <div class="flex flex-col items-center justify-center w-full h-full text-black font-hubot">
             <div class="w-4/6">
-                <div class="flex justify-between mb-1">
-                    <span class="text-base font-medium">NodeJS 3+ years</span>
-                </div>
-                <!-- <div class="w-full bg-gray-200 rounded-full h-1.5 dark:bg-gray-700">
+                <div class="w-full bg-gray-200 rounded-full h-1.5 dark:bg-gray-700">
                     <div class="bg-standard h-1.5 rounded-full w-full"></div>
-                </div> -->
+                </div>
             </div>
             <div class="w-4/6">
-                <div class="flex justify-between mb-1">
-                    <span class="text-base font-medium">TypeScript 3+ years</span>
-                </div>
-                <!-- <div class="w-full bg-gray-200 rounded-full h-1.5 dark:bg-gray-700">
+                <div class="w-full bg-gray-200 rounded-full h-1.5 dark:bg-gray-700">
                     <div class="bg-standard h-1.5 rounded-full w-full"></div>
-                </div> -->
+                </div>
             </div>
             <div class="w-4/6">
-                <div class="flex justify-between mb-1">
-                    <span class="text-base font-medium">NextJS 3+ years</span>
-                </div>
-                <!-- <div class="w-full bg-gray-200 rounded-full h-1.5 dark:bg-gray-700">
+                <div class="w-full bg-gray-200 rounded-full h-1.5 dark:bg-gray-700">
                     <div class="bg-standard h-1.5 rounded-full w-full"></div>
-                </div> -->
+                </div>
             </div>
             <div class="w-4/6">
-                <div class="flex justify-between mb-1">
-                    <span class="text-base font-medium">Docker</span>
-                </div>
-                <!-- <div class="w-full bg-gray-200 rounded-full h-1.5 dark:bg-gray-700">
+                <div class="w-full bg-gray-200 rounded-full h-1.5 dark:bg-gray-700">
                     <div class="bg-standard h-1.5 rounded-full w-full"></div>
-                </div> -->
+                </div>
             </div>
             <div class="w-4/6">
                 <div class="flex justify-between mb-1">
