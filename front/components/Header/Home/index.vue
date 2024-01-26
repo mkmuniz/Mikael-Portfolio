bb0ea8f45bee3b16214063c5ffcd8441336fd530
Mikael <mikaelmuniz2001@gmail.com>
2024-01-25 21:29:16 -0300

    fix: header & navbar

--- front/components/Header/Home/index.vue
+++ front/components/Header/Home/index.vue
@@ -1,26 +1,31 @@
 <template>
-    <div class="w-full h-screen text-white font-hubot">
-        <div class="grid sm:grid-cols-2">
-            <div class="h-screen flex justify-center items-center m-2 sm:text-left text-center">
+    <div class="w-full h-screen text-white font-hubot select-none 3xl:container 3xl:mx-auto">
+        <div class="sm:grid sm:grid-cols-2 flex flex-col items-center h-screen">
+            <div class="sm:h-screen h-full flex justify-center items-center sm:text-left text-center">
                 <span>
-                    <h1 class="text-5xl font-bold text-standard">
+                    <div class="w-full flex items-center justify-center sm:hidden visible mb-4">
+                        <NuxtImg src="/images/logo.png" class="w-2/4 max-w-[500px]" />
+                    </div>
+                    <h1 class="text-5xl 3xl:text-7xl font-bold text-standard">
                         Yo, welcome!
                     </h1>
-                    <p class="ml-2">
+                    <p class="mt-2 ml-2 text-2xl">
                         Full Stack Developer
                     </p>
                     <div class="flex space-x-3 ml-1 sm:justify-start justify-center">
-                        <NuxtLink to="https://github.com/mkmuniz" target="_blank" class="text-4xl transition-all hover:text-standard">
+                        <NuxtLink to="https://github.com/mkmuniz" target="_blank"
+                            class="text-4xl transition-all hover:text-standard">
                             <Icon name="uil:github" />
                         </NuxtLink>
-                        <NuxtLink to="https://www.linkedin.com/in/mikael-muniz-ribeiro-5764961a3/" target="_blank" class="text-4xl transition-all hover:text-standard">
+                        <NuxtLink to="https://www.linkedin.com/in/mikael-muniz-ribeiro-5764961a3/" target="_blank"
+                            class="text-4xl transition-all hover:text-standard">
                             <Icon name="uil:linkedin" />
                         </NuxtLink>
                     </div>
                 </span>
             </div>
             <div class="w-full sm:flex items-center justify-center sm:visible hidden">
-                <NuxtImg src="/images/logo.png" />
+                <NuxtImg src="/images/logo.png" class="w-2/4 max-w-[500px]" />
             </div>
         </div>
     </div>
--- front/components/Navbar/index.vue
+++ front/components/Navbar/index.vue
@@ -1,50 +1,51 @@
 <template>
-    <nav class="w-screen text-white h-full font-hubot flex">
-        <NuxtLink to="/">
-            <NuxtImg src="/images/logo.png" class="w-12 m-6" />
-        </NuxtLink>
-        <div :class="['w-full sm:flex items-center justify-end overflow-hidden sm:visible hidden']">
-            <ul class="sm:flex space-x-3 text-xl m-3">
-                <li>
-                    <NuxtLink to="#about" class="group hover:text-standard transition duration-300">
-                        ABOUT ME
-                        <span
-                            class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-standard"></span>
-                    </NuxtLink>
-                </li>
-                <li>
-                    <NuxtLink to="#experience" class="group hover:text-standard transition duration-300">
-                        EXPERIENCE
-                        <span
-                            class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-standard"></span>
-                    </NuxtLink>
-                </li>
-                <li>
-                    <NuxtLink to="#skills" class="group hover:text-standard transition duration-300">
-                        SKILLS
-                        <span
-                            class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-standard"></span>
-                    </NuxtLink>
-                </li>
-                <li>
-                    <NuxtLink to="#projects" class="group hover:text-standard transition duration-300">
-                        PROJECTS
-                        <span
-                            class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-standard"></span>
-                    </NuxtLink>
-                </li>
-                <li>
-                    <NuxtLink to="#contact" class="group hover:text-standard transition duration-300">
-                        CONTACT
-                        <span
-                            class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-standard"></span>
-                    </NuxtLink>
-                </li>
-            </ul>
-        </div>
-        <SideNavBar :status="status" />
-        <div class="sm:hidden visible w-full flex items-center justify-end">
-            <HamburgerButton :getNavBarStatus="getNavBarStatus" />
+    <nav class="text-white w-full font-hubot flex justify-center">
+        <div class="flex w-screen 3xl:container 3xl:mx-auto">
+            <NuxtLink to="/">
+                <NuxtImg src="/images/logo.png" class="w-12 m-6" />
+            </NuxtLink>
+            <div :class="['w-full sm:flex items-center justify-end overflow-hidden']">
+                <ul class="sm:visible hidden sm:flex space-x-3 text-xl m-6">
+                    <li>
+                        <NuxtLink to="#about" class="group hover:text-standard transition duration-300">
+                            ABOUT ME
+                            <span
+                                class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-standard"></span>
+                        </NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="#experience" class="group hover:text-standard transition duration-300">
+                            EXPERIENCE
+                            <span
+                                class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-standard"></span>
+                        </NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="#skills" class="group hover:text-standard transition duration-300">
+                            SKILLS
+                            <span
+                                class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-standard"></span>
+                        </NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="#projects" class="group hover:text-standard transition duration-300">
+                            PROJECTS
+                            <span
+                                class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-standard"></span>
+                        </NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="#contact" class="group hover:text-standard transition duration-300">
+                            CONTACT
+                            <span
+                                class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-standard"></span>
+                        </NuxtLink>
+                    </li>
+                </ul>
+            </div>
+            <div className="flex justify-end items-center m-6">
+                <HamburgerButton :getNavBarStatus="getNavBarStatus" />
+            </div>
         </div>
     </nav>
 </template>
