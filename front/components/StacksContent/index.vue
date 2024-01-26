58f26d8945e0ffafaea022e49ee14ba7d6751851
Mikael <mikaelmuniz2001@gmail.com>
2024-01-26 11:29:17 -0300

    fix: stacks content

--- front/components/StacksContent/index.vue
+++ front/components/StacksContent/index.vue
@@ -1,45 +1,15 @@
 <template>
     <div class="grid w-full sm:h-screen h-full bg-white font-hubot">
-        <div class="flex flex-col w-full items-center justify-center">
-            <span class="text-4xl mb-12 justify-center m-6 text-center">
+        <div class="flex flex-col w-full items-center justify-center 3xl:container 3xl:mx-auto">
+            <span class="sm:text-6xl 3xl:text-8xl text-4xl mb-12 justify-center m-6 text-center">
                 ABOUT MY CAREER & SKILLS
             </span>
             <div class="text-center m-6">
-                <p>
+                <p class="text-xl 3xl:text-3xl">
                     Actually, I have more than 3 years experience as software engineer. Working mainly
                     with NodeJS, React (and his frameworks like NextJS), GoLang & TypeScript. Also, another
                     techonologies like C#, Python, AWS, Kubernetes & DevOps in general.
                 </p>
-            </div>
-        </div>
-        <div
-            class="grid grid-cols-3 grid-rows-3 text-center align-middle place-items-center items-center sm:mb-0 mb-12 w-full h-full text-black font-hubot">
-            <div class="w-2/4 flex justify-center">
-                <NuxtImg src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5cOmlPaYYp1b9cqW_ZdX91Vt45YnKrwHDpw7GDyPlGA&s" class="w-12" />
-            </div>
-            <div class="w-2/4 flex justify-center">
-                <NuxtImg src="https://static-00.iconduck.com/assets.00/typescript-icon-2048x2048-v9sy8xic.png" class="w-12" />
-            </div>
-            <div class="w-2/4 flex justify-center">
-                <NuxtImg src="https://static-00.iconduck.com/assets.00/next-js-icon-2048x2048-5dqjgeku.png" class="w-12" />
-            </div>
-            <div class="w-2/4 flex justify-center">
-                <NuxtImg src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Python_icon_%28black_and_white%29.svg/1024px-Python_icon_%28black_and_white%29.svg.png" class="w-12" />
-            </div>
-            <div class="w-2/4 flex justify-center">
-                <NuxtImg src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmcmlzDxnwjcmd66k2YGNvPJo9ixE6K8Hyp_rVV649KA&s" class="w-12" />
-            </div>
-            <div class="w-2/4 flex justify-center">
-                <NuxtImg src="https://cdn.icon-icons.com/icons2/2622/PNG/512/brand_docker_icon_157978.png" class="w-12" />
-            </div>
-            <div class="w-2/4 flex justify-center">
-                <NuxtImg src="https://cdn.iconscout.com/icon/free/png-256/free-aws-3215369-2673787.png" class="w-12" />
-            </div>
-            <div class="w-2/4 flex justify-center">
-                <NuxtImg src="https://cdn-icons-png.flaticon.com/128/2748/2748233.png" class="w-12" />
-            </div>
-            <div class="w-2/4 flex justify-center">
-                <NuxtImg src="https://cdn-icons-png.flaticon.com/512/25/25231.png" class="w-12" />
             </div>
         </div>
     </div>
