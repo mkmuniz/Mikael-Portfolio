63a7f36b3d38bd7d201153902a407c74db84830d
Mikael <mikaelmuniz2001@gmail.com>
2024-01-27 12:24:57 -0300

    fix: skills margin

--- front/components/Skills/index.vue
+++ front/components/Skills/index.vue
@@ -2,38 +2,38 @@
     <div class="h-screen bg-white text-center font-bold" id="skills">
         <div class="h-full 3xl:container 3xl:mx-auto flex flex-col items-center justify-center">
             <h1 class="sm:text-6xl 3xl:text-8xl text-4xl">SKILLS</h1>
-            <div class="grid sm:grid-cols-3 grid-cols-2 content-center place-items-center space-y-6 w-full">
-                <div class="w-40 h-40 border border-s-4 shadow flex flex-col items-center justify-center">
+            <div class="grid sm:grid-cols-3 grid-cols-2 content-center place-items-center w-full">
+                <div class="w-40 h-40 border border-s-4 shadow flex flex-col items-center justify-center rounded mt-6">
                     <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg" class="w-16 h-16" />
                     <span class="text-xl">
                         4 years
                     </span>
                 </div>
-                <div class="w-40 h-40 border border-s-4 shadow flex flex-col items-center justify-center">
+                <div class="w-40 h-40 border border-s-4 shadow flex flex-col items-center justify-center rounded mt-6">
                     <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" class="w-16 h-16 rounded-lg" />
                     <span class="text-xl">
                         4 years
                     </span>
                 </div>
-                <div class="w-40 h-40 border border-s-4 shadow flex flex-col items-center justify-center">
+                <div class="w-40 h-40 border border-s-4 shadow flex flex-col items-center justify-center rounded mt-6">
                     <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"  class="w-16 h-16"/>
                     <span class="text-xl">
                         3 years
                     </span>
                 </div>
-                <div class="w-40 h-40 border border-s-4 shadow flex flex-col items-center justify-center">
+                <div class="w-40 h-40 border border-s-4 shadow flex flex-col items-center justify-center rounded mt-6">
                     <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original-wordmark.svg" class="w-16 h-16" />
                     <span class="text-xl">
                         2 years
                     </span>
                 </div>
-                <div class="w-40 h-40 border border-s-4 shadow flex flex-col items-center justify-center">
+                <div class="w-40 h-40 border border-s-4 shadow flex flex-col items-center justify-center rounded mt-6">
                     <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/go/go-original-wordmark.svg" class="w-16 h-16" />
                     <span class="text-xl">
                         2 years
                     </span>
                 </div>
-                <div class="w-40 h-40 border border-s-4 shadow flex flex-col items-center justify-center">
+                <div class="w-40 h-40 border border-s-4 shadow flex flex-col items-center justify-center rounded mt-6">
                     <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/amazonwebservices/amazonwebservices-original-wordmark.svg" class="w-16 h-16" />
                     <span class="text-xl">
                         2 years
