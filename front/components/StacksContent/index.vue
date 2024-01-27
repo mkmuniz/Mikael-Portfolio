32a13ad3058830a37108fb80ffcd4b0c6d0894aa
Mikael <mikaelmuniz2001@gmail.com>
2024-01-27 02:44:06 -0300

    feat: update about

--- front/components/StacksContent/index.vue
+++ front/components/StacksContent/index.vue
@@ -2,13 +2,11 @@
     <div id="about" class="grid w-full h-screen bg-white font-hubot">
         <div class="flex flex-col w-full items-center justify-center 3xl:container 3xl:mx-auto">
             <span class="sm:text-6xl 3xl:text-8xl text-4xl mb-12 justify-center m-6 text-center">
-                ABOUT MY CAREER & SKILLS
+                ABOUT
             </span>
             <div class="text-center m-6">
                 <p class="text-xl 3xl:text-3xl">
-                    Actually, I have more than 3 years experience as software engineer. Working mainly
-                    with NodeJS, React (and his frameworks like NextJS), GoLang & TypeScript. Also, another
-                    techonologies like C#, Python, AWS, Kubernetes & DevOps in general.
+                    I'm 23 years old, working as software developer since 2020 in many projects and companies. Actually, I'm Full Stack developer focused mainly on TypeScript, NodeJS, Python, NextJS (ReactJS), Go, MySQL, PostgreSQL, MongoDB & AWS/GCP.
                 </p>
             </div>
         </div>
