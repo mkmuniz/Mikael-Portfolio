eef8930d874ad0a597ba2b06d2bd45ed3f71976e
Mikael <mikaelmuniz2001@gmail.com>
2024-02-06 22:13:30 -0300

    feat: footer

--- front/components/Footer/index.vue
+++ front/components/Footer/index.vue
@@ -1,5 +1,27 @@
 <template>
-    <div class="w-full text-center font-hubot text-white bg-black sticky top-[100vh]">
-        <h1>®Made by Mikael</h1>
-    </div>
-</template>
+    <footer>
+        <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8 font-bold">
+            <div class="flex items-center justify-center">
+                <ul class="flex flex-wrap items-center 3xl:text-3xl mb-6 text-sm font-medium sm:mb-0 text-white">
+                    <li>
+                        <NuxtLink to="#" class="group hover:text-standard transition duration-300 me-4 md:me-6">ABOUT
+                        </NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink href="#" class="group hover:text-standard transition duration-300 me-4 md:me-6">GITHUB
+                        </NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink href="#" class="group hover:text-standard transition duration-300 me-4 md:me-6">LINKTREE
+                        </NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink href="#" class="group hover:text-standard transition duration-300">CONTACT</NuxtLink>
+                    </li>
+                </ul>
+            </div>
+            <hr class="my-6 sm:mx-auto lg:my-8" />
+            <span class="block text-sm text-center 3xl:text-2xl text-white">© 2024 <NuxtLink
+                    href="https://www.mkmuniz.dev" class="hover:underline 3xl:text-2xl">Mikael™</NuxtLink>. All Rights Reserved.</span>
+        </div>
+</footer></template>
