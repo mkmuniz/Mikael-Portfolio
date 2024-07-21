90c9a17fc74c2245d19c552f6153df57efcdb7bc
Mikael <mikaelmuniz2001@gmail.com>
2024-07-21 13:25:07 -0300

    fix: tanstack config error

--- front/pages/project/index.vue
+++ front/pages/project/index.vue
@@ -3,13 +3,4 @@
 </template>
 
 <script setup>
-import {  useQuery } from '@tanstack/vue-query'
-import { getAllProjects } from '~/server/api/project';
-
-// Query
-const { isPending, isError, data, error } = useQuery({
-  queryKey: ['todos'],
-  queryFn: getAllProjects,
-});
-
 </script>
