e3c997b54c4b89b7a7ab1507794603a758c9cecf
Mkmuniz <mikael.muniz@bolha.com.br>
2024-12-06 18:13:56 -0300

    fix: title to projects page

--- front/pages/project/index.vue
+++ front/pages/project/index.vue
@@ -1,13 +1,7 @@
 <template>
   <div class="projects-container">
     <div class="flex justify-between items-center mb-8">
-      <h1 class="page-title">Projetos</h1>
-      <NuxtLink 
-        to="/project/new"
-        class="px-6 py-2 bg-red-600 rounded hover:bg-red-700 transition-colors"
-      >
-        Novo Projeto
-      </NuxtLink>
+      <h1 class="page-title text-center">Projetos</h1>
     </div>
     <div class="projects-grid">
       <NuxtLink 
